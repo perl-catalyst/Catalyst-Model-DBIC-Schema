d08cea504af81941d3e2f3cbdcada41732564e6f
Brandon L. Black <blblack@gmail.com>
2006-02-27 23:59:15 +0000

    0.08, and the removal of the halfhearted ->require attempt

--- lib/Catalyst/Model/DBIC/Schema.pm
+++ lib/Catalyst/Model/DBIC/Schema.pm
@@ -6,7 +6,7 @@ use NEXT;
 use UNIVERSAL::require;
 use Carp;
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 
 __PACKAGE__->mk_classaccessor('composed_schema');
 __PACKAGE__->mk_accessors('schema');
@@ -185,12 +185,6 @@ sub new {
     no strict 'refs';
     foreach my $moniker ($self->schema->sources) {
         my $classname = "${class}::$moniker";
-	$classname->require;
-        if($@ && $@ !~ /^Can't locate /) {
-            croak "Failed to load external class definition"
-                  . "for '$classname': $@";
-        }
-
         *{"${classname}::ACCEPT_CONTEXT"} = sub {
             shift;
             shift->model($model_name)->resultset($moniker);
