1714a8a0ad486c7956ce7bd3f62e7b78a3d7222e
Rafael Kitover <rkitover@cpan.org>
2011-10-25 15:20:04 -0400

    release 0.58

--- lib/Catalyst/Helper/Model/DBIC/Schema.pm
+++ lib/Catalyst/Helper/Model/DBIC/Schema.pm
@@ -4,7 +4,7 @@ use namespace::autoclean;
 use Moose;
 no warnings 'uninitialized';
 
-our $VERSION = '0.57';
+our $VERSION = '0.58';
 $VERSION = eval $VERSION;
 
 use Carp;
--- lib/Catalyst/Model/DBIC/Schema.pm
+++ lib/Catalyst/Model/DBIC/Schema.pm
@@ -5,7 +5,7 @@ use mro 'c3';
 extends 'Catalyst::Model';
 with 'CatalystX::Component::Traits';
 
-our $VERSION = '0.57';
+our $VERSION = '0.58';
 $VERSION = eval $VERSION;
 
 use namespace::autoclean;
