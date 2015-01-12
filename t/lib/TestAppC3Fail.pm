d4f87e637e41e409eaa939c6bd384d7cf0a5258d
Dagfinn Ilmari Mannsåker <ilmari@ilmari.org>
2015-01-12 12:40:55 +0000

    Ignore config key added by Catalyst::Runtime 5.90080 (RT#101446)

--- t/lib/TestAppC3Fail.pm
+++ t/lib/TestAppC3Fail.pm
@@ -16,10 +16,15 @@ my @keys = sort keys( %{ __PACKAGE__->config } );
 
 __PACKAGE__->setup;
 
-my @new_keys = sort keys( %{ __PACKAGE__->config } );
+my @new_keys = sort
+    # Ignore key added by horrid hack in Catalyst::Runtime 5.90080
+    grep { $_ ne '__configured_from_psgi_middleware'}
+    keys( %{ __PACKAGE__->config } );
+
 use Test::More;
 
-is_deeply(\@new_keys, \@keys, 'Config keys correct');
+is_deeply(\@new_keys, \@keys, 'Config keys correct')
+    or diag explain [\@keys, \@new_keys];
 
 1;
 
